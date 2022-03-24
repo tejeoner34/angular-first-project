c7c276ce442f36ad3a6195423fc320cfbbf18f71
Alvaro Tejedor <alvaro.tejedorzarco@nttdata.com>
2022-03-24 17:39:02 +0100

    file path modification

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -9,7 +9,7 @@ export class HeaderComponent implements OnInit {
 
 
   isPlaying: boolean = false
-  audio = new Audio("../../../assets/music/Harry Potter Theme Song.mp3")
+  audio = new Audio("assets/music/Harry Potter Theme Song.mp3")
 
   constructor() { }
 
